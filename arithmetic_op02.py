94d9de2eff6f5bde3e3c1ed3af27164478fb4c64
umid <umidxonisroilov@gmail.com>
2023-10-19 12:44:04 +0500

    arithmetic

--- arithmetic_op02.py
+++ arithmetic_op02.py
@@ -1,5 +1,7 @@
 #Create a varaible 'number' and assign it the value of 43.
-
+number=43
 #Create a variable 'answer' and assign it the sum of the number's digits.
-
+n1 = 3 
+n2 = 4
+print(n1+n2)
 #Print the vallue of the 'answer'.
