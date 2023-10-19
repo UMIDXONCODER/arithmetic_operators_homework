c817c05079036324e86331aee1c00c93384479a7
umid <umidxonisroilov@gmail.com>
2023-10-19 21:38:09 +0500

    umid

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,7 +1,9 @@
 #Create a variable called 'number' and assign it the two-digit number
-
+number=23
 #Find the reverse of the number and assign it to a variable called 'answer'.
-
+n1=number%10
+n2=number//10
+print(n1*20+3)
 #Print the value of the 'answer'
 
 
